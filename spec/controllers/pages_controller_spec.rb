6bef60999ad1a98ee040c3f8f579a88f34f62936
Jason Leng <cool_curly@hotmail.com>
2011-08-12 22:16:32 -0700

    A little dynamic

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,9 +5,8 @@ describe PagesController do
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
-      response.should be_success   
-      
-    end   
+      response.should be_success     
+    end    
     
   end
 
@@ -21,6 +20,13 @@ describe PagesController do
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
+      response.should be_success
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
       response.should be_success
     end
   end
