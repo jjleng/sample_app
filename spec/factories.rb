a1f7d84d060978eb9cfae9aeed7fa4cb490a2d43
Jason Leng <cool_curly@hotmail.com>
2011-08-17 00:00:02 -0700

    User model with passwords, and user show page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,2 +1,6 @@
 Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
 end
